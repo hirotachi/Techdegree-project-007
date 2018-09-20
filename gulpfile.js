768708a7100f1dddc25fba84e58707d592553800
Hirotachi <hirotachi@epical.me>
2018-09-20 05:51:38 +0100

    Setup workenv gulp sass and browserSync and html

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require("gulp"),
+      sass = require("gulp-sass"),
+      browserSync = require("browser-sync").create();
+
+
+
+gulp.task("serve", ["sass"], function(){
+    browserSync.init({
+        notify: false,
+        server: "./app"
+    });
+
+    gulp.watch("app/*.html").on("change", browserSync.reload);
+
+    gulp.watch("app/scss/*/*.scss", ["sass"]);
+});
+
+gulp.task("sass", function(){
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .on("error", function(err){
+            console.log(err.toStrong());
+            this.emit("end");
+        })
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("default", ["serve"]);
